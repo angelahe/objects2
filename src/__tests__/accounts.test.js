2cf13662cf404f2baefd7c43c098e726df0a4552
angela <angela@aviak.ca>
2019-03-11 22:13:30 -0600

    add accounts class and accounts tests

---
+++ src/__tests__/accounts.test.js
@@ -0,0 +1,137 @@
+import Accounts from '../components/Accounts';
+import Account from '../components/account';
+
+test("Test AccountController class", () => {
+    console.log("Test AccountController constructor");
+    const newAccountController = new Accounts();
+    expect(newAccountController).toEqual({"Accounts":[]});
+    const newAccount = new Account(10, "Sue", 0);
+    const newAccount2 = new Account(15, "John", 1);
+    const newAccount3 = new Account(25, "Andrew", 2);
+    console.log("Add a new acccount to the Account Controller");
+    newAccountController.addAccount(newAccount);
+    expect(newAccountController).not.toBe({"Accounts": []});
+    expect(newAccountController.Accounts.length).toBe(1);
+    //console.log("AccountController is", newAccountController);
+    //console.log("1st account is", newAccountController.Accounts[0]);
+    expect(newAccountController.Accounts[0].acctName).toBe("Sue");
+    expect(newAccountController.Accounts[0].balance).toBe(10);
+    newAccountController.addAccount(new Account(15, "John"));
+    expect(newAccountController.Accounts.length).toBe(2);
+    console.log("Test Account delete of 1st account in array of 2");
+    expect(newAccountController.deleteAccount(0).length).toBe(1);
+    expect(newAccountController.Accounts[0].acctName).toBe("John");
+    expect(newAccountController.Accounts[0].balance).toBe(15);
+    console.log("accounts is", newAccountController.Accounts);
+    console.log("Test Account delete of first account of 1, return empty array");
+    expect(newAccountController.deleteAccount(0).length).toBe(0);
+    console.log("Test Account delete of middle account in array of 3");
+    newAccountController.addAccount(newAccount);
+    newAccountController.addAccount(newAccount2);
+    newAccountController.addAccount(newAccount3);
+    newAccountController.deleteAccount(1);
+    expect(newAccountController.Accounts.length).toBe(2);
+    expect(newAccountController.Accounts[0].acctName).toBe("Sue");
+    expect(newAccountController.Accounts[1].acctName).toBe("Andrew");
+    console.log("Test Account delete from end of array");
+    expect(newAccountController.deleteAccount(1).length).toBe(1);
+    expect(newAccountController.Accounts[0].acctName).toBe("Sue");
+
+});
+
+test("Test AccountController getMinAccount module", () => {
+    const newAccount3 = new Account(25, "Andrew", 0 );
+    const newAccount1 = new Account(10, "Sue", 1);
+    const newAccount2 = new Account(15, "John", 2);
+    const newAccount4 = new Account(5, "Mary", 3);
+    const newAccountController = new Accounts();
+    console.log("With empty accounts array returns Infinity");
+    expect(newAccountController.getMinAccount()).toBe(Infinity);
+    newAccountController.addAccount(newAccount3);
+    console.log("Test that with an array with 1 item returns 0");
+    expect(newAccountController.getMinAccount()).toBe(0);
+    newAccountController.addAccount(newAccount1);
+    console.log("Test that an array of 2 items with 2nd item as minimum returns 1");
+    expect(newAccountController.getMinAccount()).toBe(1);
+    newAccountController.addAccount(newAccount2);
+    expect(newAccountController.getMinAccount()).toBe(1);
+    newAccountController.addAccount(newAccount4);
+    console.log("Test that last item in array is the minimum ie returns 3");
+    expect(newAccountController.getMinAccount()).toBe(3);
+});
+
+test("Test AccountController getmaxAccount module", () => {
+    const newAccount1 = new Account(10, "Sue", 0);
+    const newAccount2 = new Account(15, "John", 1);
+    const newAccount3 = new Account(45, "Andrew", 2);
+    const newAccount4 = new Account(35, "Mary", 3);
+    const newAccountController = new Accounts();
+    console.log("With empty accounts array returns Infinity");
+    expect(newAccountController.getMaxAccount()).toBe(Infinity);
+    newAccountController.addAccount(newAccount1);
+    console.log("With one account returns 0 as index of highest account");
+    expect(newAccountController.getMaxAccount()).toBe(0);
+    newAccountController.addAccount(newAccount2);
+    console.log("With two accounts and last one is the highest returns index 1");
+    expect(newAccountController.getMaxAccount()).toBe(1);
+    newAccountController.addAccount(newAccount3);
+    newAccountController.addAccount(newAccount4);
+    console.log("returns index in middle of array, in this case 2 as highest index");
+    expect(newAccountController.getMaxAccount()).toBe(2);
+});
+
+test("Test AccountController getAccountTotal module", () => {
+    const newAccount1 = new Account(10, "Sue", 0);
+    const newAccount2 = new Account(15, "John", 1);
+    const newAccount3 = new Account(45, "Andrew", 2);
+    const newAccount4 = new Account(35, "Mary", 3);
+
+    const newAccountController = new Accounts();
+    console.log("With empty accounts array returns Zero");
+    expect(newAccountController.getAccountTotal()).toBe(0);
+    newAccountController.addAccount(newAccount1);
+    console.log("With one account will return the balance of that account");
+    expect(newAccountController.getAccountTotal()).toBe(newAccountController.Accounts[0].balance);
+    newAccountController.addAccount(newAccount2);
+    newAccountController.addAccount(newAccount3);
+    newAccountController.addAccount(newAccount4);
+    expect(newAccountController.getAccountTotal()).toBe(105);
+
+});
+
+//withdraw from account allows for a balance to be negative, returns true if successful
+test("Test AccountController withdrawFromAccount module", () => {
+    const newAccount1 = new Account(10, "Sue", 0);
+    const newAccount2 = new Account(15, "John", 1);
+
+    const newAccountController = new Accounts();
+    console.log("With empty accounts array returns false");
+    expect(newAccountController.withdrawFromAccount()).toBe(false);
+    newAccountController.addAccount(newAccount1);
+    expect(newAccountController.withdrawFromAccount(0, 10)).toBe(true);
+    expect(newAccountController.Accounts[0].balance).toBe(0);
+    expect(newAccountController.withdrawFromAccount(0, 10)).toBe(true);
+    expect(newAccountController.Accounts[0].balance).toBe(-10);
+    newAccountController.addAccount(newAccount2);
+    expect(newAccountController.withdrawFromAccount(1, 10)).toBe(true);
+    expect(newAccountController.Accounts[1].balance).toBe(5);
+});
+
+//deposit to account allows for balance to go from negative to positive, return true if
+//deposit is successful, false if fails
+test("Test AccountController depositToAccount module", () => {
+    const newAccount1 = new Account(-10, "Sue", 0);
+    const newAccount2 = new Account(15, "John", 1);
+
+    const newAccountController = new Accounts();
+    console.log("With empty accounts array returns false");
+    expect(newAccountController.depositToAccount()).toBe(false);
+    newAccountController.addAccount(newAccount1);
+    expect(newAccountController.depositToAccount(0, 10)).toBe(true);
+    expect(newAccountController.Accounts[0].balance).toBe(0);
+    expect(newAccountController.depositToAccount(0, 10)).toBe(true);
+    expect(newAccountController.Accounts[0].balance).toBe(10);
+    newAccountController.addAccount(newAccount2);
+    expect(newAccountController.depositToAccount(1, 10)).toBe(true);
+    expect(newAccountController.Accounts[1].balance).toBe(25);
+});
---
+++ src/components/accounts.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+class Accounts {
+
+    constructor() {
+        this.Accounts = [];
+    }
+
+    addAccount(newAccount) {
+        console.log("in addAccount");
+        this.Accounts.push(newAccount);
+        console.log("accounts is", this.Accounts);
+        return this.Accounts;
+    }
+
+    deleteAccount(index) {
+        this.Accounts.splice(index, 1);
+        return this.Accounts;
+    }
+
+    //update the account balance of the account, return true on success, false on fail
+    withdrawFromAccount(index = -1, amount = 0) {
+
+        //on a non null account, withdraw the amount
+        if ((this.Accounts.length > index) &&  (index >= 0)) {
+            this.Accounts[index].withdraw(amount);
+            return(true);
+        }
+        else
+            return(false);
+
+    }
+
+    //update the account balance of the account, return true on success, false on fail
+    depositToAccount(index, amount) {
+        if ((this.Accounts.length > index) &&  (index >= 0)) {
+            this.Accounts[index].deposit(amount);
+            return(true);
+        }
+        else
+            return(false);
+    }
+
+    //returns the index to the account with the lowest balance, or first lowest item if tied
+    getMinAccount() {
+
+        let len = this.Accounts.length, min = Infinity, index = Infinity;
+
+        while (len--) {
+            if (this.Accounts[len].balance < min) {
+                min = this.Accounts[len].balance;
+                index = len;
+            }
+        }
+        return(index);
+
+    }
+
+    //returns the index to the account with the highest balance, or first highest item if tied
+    getMaxAccount() {
+
+        let len = this.Accounts.length, max = 0, index = Infinity;
+
+        while (len--) {
+            if (this.Accounts[len].balance > max) {
+                max = this.Accounts[len].balance;
+                index = len;
+            }
+        }
+        return(index);
+
+
+        //return(0);
+    }
+
+    getAccountTotal() {
+
+        let len = this.Accounts.length,  total = 0;
+
+        while (len--) {
+            if (this.Accounts[len].balance > 0) {
+                total+= this.Accounts[len].balance;
+            }
+        }
+
+        return(total);
+    }
+
+
+}
+
+export default Accounts;
