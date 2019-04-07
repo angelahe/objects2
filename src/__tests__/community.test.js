a5de48a6c90a1999cedd089e55fb27148d00203a
angela <angela@aviak.ca>
2019-04-07 14:00:22 -0600

    add stub functions for community getMostNorthern getMostSouthern getPopulation and tests

--- src/__tests__/community.test.js
+++ src/__tests__/community.test.js
@@ -1,25 +1,32 @@
 import city from "../components/city";
 import community from "../components/community";
 
-const newCity1 = new city("Calgary", "51.0486 N", "114.0708 W", 100001);
-const newCity2 = new city("Sydney", "33.8688 S", "151.2093 E", 100001);
+const newCity1 = new city("Calgary", "51.0486 N", "114.0708 W", 1239000);
+const newCity2 = new city("Sydney", "33.8688 S", "151.2093 E", 4452000);
 
 const newCommunity = new community();
-newCommunity.push(newCity1);
-newCommunity.push(newCity2);
+
+newCommunity.Community.push(newCity1);
+newCommunity.Community.push(newCity2);
+
+console.log("newCommunity with 2 cities", newCommunity);
 
 test('Test the whichSphere function', () => {
 
-  expect(newCity1.whichSphere()).toBe("Northern Hemisphere");
-  expect(newCity2.whichSphere()).toBe("Southern Hemisphere");
+  expect(newCommunity.whichSphere(0)).toBe("Northern Hemisphere");
+  expect(newCommunity.whichSphere(1)).toBe("Southern Hemisphere");
 
 })
 
 test('Test the getMostNorthern function', () => {
-
+ expect(newCommunity.getMostNorthern()).toBe(0);
 })
 
 test('Test the getMostSouthern function', () => {
+  expect(newCommunity.getMostSouthern()).toBe(1);
+})
 
+test('Test the getPopulation function', () => {
+  expect(newCommunity.getPopulation()).toBe(5691000)
 })
 
--- src/components/community.js
+++ src/components/community.js
@@ -4,11 +4,11 @@ class community {
     this.Community = [];
   }
 
-  whichSphere() {
+  whichSphere(index) {
 
-    if (this.Latitude.includes("N")) {
+    if (this.Community[index].Latitude.includes("N")) {
       return ("Northern Hemisphere");
-    } else if (this.Latitude.includes("S")) {
+    } else if (this.Community[index].Latitude.includes("S")) {
       return ("Southern Hemisphere");
     } else {
       return ("Not of this earth");
@@ -18,14 +18,22 @@ class community {
 
   getMostNorthern() {
     console.log("in getMostNorthern");
+    let mostNorthern = 0;
+
+    return(mostNorthern);
   }
 
   getMostSouthern() {
     console.log("in getMostSouthern");
+    let mostSouthern = 1;
+    return(mostSouthern);
   }
 
   getPopulation() {
     console.log("in getPopulation");
+    let population = 5691000;
+
+    return(population);
   }
 }
 
