ea241b23aafcabcbe3fbbf5a25a70b5f504dbef9
angela <angela@aviak.ca>
2019-04-15 11:07:41 -0600

    change app icons and add react app back to default

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-//import Starter from './components/Starter';
+import Starter from './components/Starter';
 import MathComp from './components/MathComp';
 import AccountUI from './components/AccountUI';
 import AccountsUI from './components/AccountsUI';
@@ -13,13 +13,12 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            navClicked: "linked",
+            navClicked: "default",
             message: ""
         };
     }
 
     onNavClickChanged = (clickBtn) => {
-//        console.log("in onNavClickChanged, clickbtn is", clickBtn);
         this.setState({navClicked : clickBtn, message : clickBtn + " was clicked"});
     }
 
@@ -29,11 +28,13 @@ class App extends Component {
 
             <div className="App">
                 <NavBar navClicked={this.onNavClickChanged}/>
-                {(this.state.navClicked==="linked") ? <LinkedListUI /> : null}
+                {(this.state.navClicked==="default") ? <Starter /> : null}
                 {(this.state.navClicked==="math") ? <MathComp /> : null}
                 {(this.state.navClicked==="account") ? <AccountUI /> : null}
                 {(this.state.navClicked==="accounts") ? <AccountsUI /> : null}
                 {(this.state.navClicked==="community") ? <CommunityUI/> : null}
+                {(this.state.navClicked==="linked") ? <LinkedListUI /> : null}
+                {(this.state.navClicked==="queue") ? <CommunityUI/> : null}
             </div>
         );
     }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,19 @@
 import React from "react";
 import '../App.css';
 
-import analytics from '../images/analytics.svg';
-import creative from '../images/creative.svg';
-import leader from '../images/leader.svg';
-import teamwork from '../images/teamwork.svg';
-import teamwork2 from '../images/teamwork2.svg';
+//import analytics from '../images/analytics.svg';
+//import creative from '../images/creative.svg';
+//import leader from '../images/leader.svg';
+//import teamwork from '../images/teamwork.svg';
+//import teamwork2 from '../images/teamwork2.svg';
+
+import react from '../images/react_FFFFFF.png'
+import calculate from '../images/calculate_FFFFFF.png'
+import account from '../images/account_FFFFFF.png'
+import accounts from '../images/accounts2_FFFFFF.png'
+import city from '../images/city_FFFFFF.png'
+import link from '../images/link_FFFFFF.png'
+import queue from '../images/queue_FFFFFF.png'
 
 const NavBar = (props) => {
 
@@ -18,11 +26,14 @@ const NavBar = (props) => {
     return (
         <div>
             <header>
-                <img src={analytics} navbtn="linked" onClick={handleClick} className="App-icon" alt="one"/>
-                <img src={creative} navbtn="math" onClick={handleClick} className="App-icon" alt="two"/>
-                <img src={leader} navbtn="account" onClick={handleClick} className="App-icon" alt="three"/>
-                <img src={teamwork} navbtn="accounts" onClick={handleClick} className="App-icon" alt="four"/>
-                <img src={teamwork2} navbtn="community" onClick={handleClick} className="App-icon" alt="five"/>
+                <img src={react} navbtn="default" onClick={handleClick} className="App-icon" alt="react"/>
+                <img src={calculate} navbtn="math" onClick={handleClick} className="App-icon" alt="calculate"/>
+                <img src={account} navbtn="account" onClick={handleClick} className="App-icon" alt="account"/>
+                <img src={accounts} navbtn="accounts" onClick={handleClick} className="App-icon" alt="accounts"/>
+                <img src={city} navbtn="community" onClick={handleClick} className="App-icon" alt="city"/>
+                <img src={link} navbtn="linked" onClick={handleClick} className="App-icon" alt="linked list"/>
+                <img src={queue} navbtn="queue" onClick={handleClick} className="App-icon" alt="queue"/>
+
             </header>
             <p>{props.message}</p>
         </div>
